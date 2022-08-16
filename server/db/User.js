1a8b2ed1675ae8172bf92b72a64fee73bf97bf1a
Alec B <alec.butterfield1@gmail.com>
2022-08-16 09:13:21 -0700

    Most of tier 2 specs functional, small attempt at other tiers

--- server/db/User.js
+++ server/db/User.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const { database } = require('../../../../Checkpoint- Sequelize and Express Public/Checkpoint-Express-Sequelize-B/config');
 const db = require('./db');
 
  // Add your Sequelize fields here
@@ -21,16 +22,58 @@ const User = db.define('user', {
   },
   mentorId: {
     type: Sequelize.INTEGER
+  },
+  isStudent: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      if (this.userType === 'STUDENT'){
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  isTeacher: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      if (this.userType === 'TEACHER'){
+        return true
+      } else {
+        return false
+      }
+    }
   }
-
-
 });
+
+/*User.afterUpdate(user => {
+  let mentor = user.mentorId
+  let teacherCheck =  User.findAll({where: {id: mentor}})
+  console.log(mentor)
+  console.log(teacherCheck)
+  if (teacherCheck.isStudent === true) {
+    if (user.mentorId !== null) {
+      user.mentorId = null
+      throw console.error();
+    }
+  }
+})*/
 
 User.findUnassignedStudents = async function() {
   let studentsWithoutMentor = await User.findAll({where: {mentorId: null, userType: 'STUDENT'}})
   return studentsWithoutMentor
 }
 
+
+User.findTeachersAndMentees = async function(){
+  let teachers = (await User.findAll({where: {userType: 'TEACHER'}}))
+  for (let i = 0; i < teachers.length; i++){
+    let curTeacher = teachers[i]
+    let curTeacherIdx = curTeacher.id
+    let curStudents = await User.findAll({where: {mentorId: curTeacherIdx }})
+    curTeacher.mentees = curStudents
+  }
+  return teachers
+}
 
 /**
  * We've created the association for you!
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const db = require('../db');
 const {
   models: { User },
 } = require('../db');
@@ -18,9 +19,57 @@ const {
 
 // Add your routes here:
 
-router.get('/unassigned', async (req, res, ) => {
+router.get('/unassigned', async (req, res) => {
   let unassignedStudents =  await User.findUnassignedStudents()
   res.send(unassignedStudents)
 })
+
+router.get('/teachers', async (req, res) => {
+  let teachersAndMentees = await User.findTeachersAndMentees()
+  res.send(teachersAndMentees)
+})
+
+//NOT COMPLETE!!
+router.put('/users/:id', async (req,res) => {
+  let findUser = User.findAll({where: {id:req.params.id}})
+  if (findUser === undefined){
+    res.sendStatus(404)
+  } else {
+    res.status(200).send(findUser)
+  }
+})
+
+
+//NOT COMPLETE!!
+router.post('/users', async (req, res) => {
+  let newUser = req.body.name
+  let userCheck = await User.findAll({where: {name: newUser}})
+  console.log(userCheck)
+  if (userCheck) {
+    res.sendStatus(409)
+  } else {
+    res.status(201).send(newUser)
+  }
+
+})
+
+//NOT COMPLETE!!!
+router.delete('/:id', async (req, res, next) => {
+  let curUser = await User.findAll({where: {id: req.params.id}})
+  next()
+  if (curUser === false){
+    res.sendStatus(404)
+  }
+  if (curUser.id !== req.params.id) {
+    res.sendStatus(400)
+  } else {
+    res.status(204).send(curUser)
+  }
+})
+
+router.use((err, req, res, next) => {
+  res.status(500).send(err.message)
+})
+
 
 module.exports = router;
